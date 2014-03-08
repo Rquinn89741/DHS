4951ad7253333570d4b58a2cb6bc8dc1afd0637b
Ryan <rquinn89741@gmail.com>
2014-03-08 00:02:23 -0600

    Articles added

--- DHS/src/com/ryanq/dhs/ArticleActivity.java
+++ DHS/src/com/ryanq/dhs/ArticleActivity.java
@@ -1,9 +1,22 @@
 package com.ryanq.dhs;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
 import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.Html.ImageGetter;
 import android.util.Log;
 import android.view.Menu;
 import android.widget.TextView;
@@ -11,16 +24,27 @@ import android.widget.TextView;
 public class ArticleActivity extends Activity {
 	String header;
 	String message;
+	ArrayList<String> links;
+	ArrayList<Drawable> drawables = new ArrayList<Drawable>();
+	int i = 0;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+
 		super.onCreate(savedInstanceState);
 		Intent intent = getIntent();
 		header = intent.getStringExtra("title");
 		message = intent.getStringExtra("article");
+		links = intent.getStringArrayListExtra("links");
 		setContentView(R.layout.activity_article);
 		fillTitle();
 		fillArticle();
+		try {
+			new DownloadFilesTask().execute(new URL("http://www.random.net/"));
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
@@ -33,6 +57,7 @@ public class ArticleActivity extends Activity {
 		ActionBar bar = getActionBar();
 		bar.setDisplayHomeAsUpEnabled(true);
 		bar.setDisplayShowTitleEnabled(false);
+
 		return true;
 	}
 
@@ -47,8 +72,82 @@ public class ArticleActivity extends Activity {
 	public void fillArticle() {
 
 		TextView article = (TextView) findViewById(R.id.article);
+
 		Log.d("msg", message);
-		article.setText(message);
+		article.setText(Html.fromHtml(message, new ImageGetter() {
+			@Override
+			public Drawable getDrawable(String source) {
+				Drawable drawFromPath = getResources().getDrawable(
+						R.drawable.test);
+
+				drawFromPath.setBounds(0, 0, drawFromPath.getIntrinsicWidth(),
+						drawFromPath.getIntrinsicHeight());
+				// if (i < links.size())
+				// i++;
+				return drawFromPath;
+			}
+		}, null));
+		// article.setText(Html.fromHtml(message));
 
 	}
+
+	private class DownloadFilesTask extends AsyncTask<URL, Bitmap, Long> {
+
+		@Override
+		protected void onPreExecute() {
+			super.onPreExecute();
+
+			// get the URL's to
+
+		}
+
+		@Override
+		protected Long doInBackground(URL... urls) {
+			/*
+			 * if (links.size() > 0) { for (String link : links) {
+			 * publishProgress(getBitmap(link));
+			 * 
+			 * } }
+			 */
+			return null;
+
+		}
+
+		@Override
+		protected void onProgressUpdate(Bitmap... data) {
+
+			Drawable draw = new BitmapDrawable(getResources(), data[0]);
+			drawables.add(draw);
+			;
+		}
+
+		@Override
+		protected void onPostExecute(Long result) {
+
+		}
+
+	}
+
+	public static Bitmap getBitmap(String link) {
+		/*--- this method downloads an Image from the given URL, 
+		 *  then decodes and returns a Bitmap object
+		 ---*/
+
+		try {
+			URL url = new URL(link);
+			HttpURLConnection connection = (HttpURLConnection) url
+					.openConnection();
+			connection.setDoInput(true);
+			connection.connect();
+			InputStream input = connection.getInputStream();
+			Bitmap bitmap = BitmapFactory.decodeStream(input);
+
+			return bitmap;
+
+		} catch (IOException e) {
+
+			return null;
+		}
+	}
+
 }
--- DHS/src/com/ryanq/dhs/MainActivity.java
+++ DHS/src/com/ryanq/dhs/MainActivity.java
@@ -1,15 +1,22 @@
 package com.ryanq.dhs;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.List;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.nodes.Node;
 import org.jsoup.select.Elements;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
@@ -26,30 +33,29 @@ public class MainActivity extends Activity {
 	private ListView listView;
 	private final ArrayList<String> titles = new ArrayList<String>();
 	private final ArrayList<String> articles = new ArrayList<String>();
+	private final ArrayList<ArrayList<String>> links = new ArrayList<ArrayList<String>>();
 	private ArrayAdapter adapter;
+	private final ArrayList<String> watwat = new ArrayList<String>();
+	boolean loaded = false;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 
 		try {
+
 			new DownloadFilesTask().execute(new URL("http://random.net"));
 
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		}
 
-		// for (String s : titles) {
-
-		// if (s != null)
-		// Log.d("wat", s);
-		// }
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		listView = (ListView) findViewById(R.id.listview);
 		article = new Intent(this, ArticleActivity.class);
 		adapter = new ArrayAdapter<String>(this,
 				android.R.layout.simple_list_item_1, titles);
+
+		;
 
 		listView.setAdapter(adapter);
 		listView.setOnItemClickListener(itemClickedHandler);
@@ -71,6 +77,8 @@ public class MainActivity extends Activity {
 				long id) {
 			article.putExtra("title", titles.get(position));
 			article.putExtra("article", articles.get(position));
+			// if (links.get(position) != null)
+			// article.putExtra("imageLinks", links.get(position));
 
 			startActivity(article);
 		}
@@ -124,8 +132,11 @@ public class MainActivity extends Activity {
 					Log.d("procedure", "starting page check");
 					if (d != null) {
 						String a = d.select(".sw-newsHeader").text();
+						Log.d("title", a);
+						Log.d("procedure", "run page check");
 
 						String b = getArticle(d);
+						//
 						publishProgress(a, b);
 
 					}
@@ -141,50 +152,98 @@ public class MainActivity extends Activity {
 
 		@Override
 		protected void onProgressUpdate(String... data) {
-			// progress info
+
+			// progress info, loading spinner code will also go here
+			if (data[1] != null)
+				Log.d("data", data[1]);
 
 			if (data[0] != null) {
 				titles.add(data[0]);
-				Log.d("updsate", "added item to titles");
+				Log.d("update", "added item to titles");
 
 			}
 			if (data[1] != null) {
 				articles.add(data[1]);
 				Log.d("update", "added item to articles");
 				for (String a : articles) {
-					Log.d("update", a.substring(0, 50));
 				}
 			}
+			if (articles.get(0) != null)
+				Log.d("data", articles.get(0));
 			adapter.notifyDataSetChanged();
 		}
 
 		@Override
 		protected void onPostExecute(Long result) {
 			// at the end
+			Log.d("data", articles.toString());
+			loaded = true;
 		}
 
 		protected String getArticle(Document doc) {
 
-			Log.d("r", "running article code)");
-			Element table = doc.select(".sw-newsHeader").get(0).parent()
-					.parent().parent();
-			Elements s = table.select("p");
-			Log.d("s", s.text());
-			if (s.text().length() > 0)
-				return s.text();
-			/*
-			 * for (Element para : s) { Log.d("r", "searching element " +
-			 * para.text()); if (para.text().length() > 0 &&
-			 * (para.previousSibling().previousSibling() .previousSibling()
-			 * .equals(doc.select(".sw-newsHeader")) || para
-			 * .previousSibling().previousSibling()
-			 * .equals(doc.select(".sw-newsHeader")))) {
-			 * 
-			 * return para.text(); }
-			 */
-			return null;
+			Log.d("procedure", "getting article");
+			Element td = doc.select(".sw-newsHeader").get(0).parent();
+			Elements kids = td.children();
 
+			Elements images = td.select("img");
+			String article = "";
+
+			List<Node> nodes = td.childNodes();
+			ArrayList<String> imageLinks = new ArrayList<String>();
+			if (doc.select(".sw-newsHeader").text()
+					.equals("LadyCats Advanced to the State Championships!"))
+				Log.d("channel1", nodes.toString() + 0);
+			for (int n = 0; n < nodes.size(); n++) {
+
+				if (images.size() > 0) {
+
+					for (int a = 0; a < images.size(); a++) {
+						if (nodes.get(n).outerHtml()
+								.contains(images.get(a).attr("src"))) {
+
+							imageLinks.add(images.get(a).absUrl("src"));
+						}
+
+					}
+
+				}
+
+				if (!nodes.get(n).outerHtml().contains("sw-newsHeader"))
+
+					article += nodes.get(n).toString();
+			}
+			watwat.add(doc.select(".sw-newsHeader").text());
+			links.add(imageLinks);
+			return article;
 		}
-
 	}
+
+	public static Bitmap getBitmap(String link) { // will use this to grab the
+													// images? Can probably be
+													// moved to ArticleActivity
+													// assuming no image
+													// thumbnails are used for
+													// the listview
+		/*--- this method downloads an	 Image from the given URL, 
+		 *  then decodes and returns a Bitmap object
+		 ---*/
+
+		try {
+			URL url = new URL(link);
+			HttpURLConnection connection = (HttpURLConnection) url
+					.openConnection();
+			connection.setDoInput(true);
+			connection.connect();
+			InputStream input = connection.getInputStream();
+			Bitmap bitmap = BitmapFactory.decodeStream(input);
+
+			return bitmap;
+
+		} catch (IOException e) {
+
+			return null;
+		}
+	}
+
 }
